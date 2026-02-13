a39e029ef15668d66f81cb86e2b5eab39f93102d
Eric Binnendyk <ericabq@gmail.com>
2026-02-12 22:45:35 -0800

    add more tests for prime and composite mnemonics and feedback

--- app/prime/feedbackCheck.test.tsx
+++ app/prime/feedbackCheck.test.tsx
@@ -2,23 +2,71 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Page from './page';
+import { isPrime } from './lib/numberGenerator';
 
 describe('FeedbackCheck', () => {
-  it ("passes if feedback is displayed", async () => {
+  it ("passes if yes/no feedback is displayed", async () => {
     render(<Page />);
     const user = userEvent.setup();
 
     // Start the game and click "Yes"
     await user.click(screen.getByText('Start'));
+    const num1 = parseInt(document.getElementById('num').textContent);
     await user.click(screen.getByText('Yes'));
-    const feedback = await screen.findByText(/it's/i); // short for (Yes|No) it's (prime|composite)
+    let feedback;
+    if (isPrime(num1)) {
+      feedback = await screen.findByText("Correct!");
+    }
+    else {
+      feedback = await screen.findByText("Incorrect!");
+    }
     expect(feedback).toBeInTheDocument();
+
+    const num2 = parseInt(document.getElementById('num').textContent);
 
     // Click "No"
     await user.click(screen.getByText('No'));
-    const feedback2 = await screen.findByText(/it's/i); // short for (Yes|No) it's (prime|composite)
-    expect(feedback2).toBeInTheDocument();
+    if (isPrime(num2)) {
+      feedback = await screen.findByText("Incorrect!");
+    }
+    else {
+      feedback = await screen.findByText("Correct!");
+    }
+    expect(feedback).toBeInTheDocument();
   });
 
-  // to do: add one that tests the explanation of why the number is prime or composite, once I add that
+  it ("passes if the number is correctly reported to be prime or composite", async () => {
+    render(<Page />);
+    const user = userEvent.setup();
+
+    // Start the game and click "Yes"
+    await user.click(screen.getByText('Start'));
+    const num = parseInt(document.getElementById('num').textContent);
+    await user.click(screen.getByText('Yes'));
+    let feedback;
+    if (isPrime(num)) {
+      feedback = await screen.getByText(num + " is prime");
+    }
+    else {
+      feedback = await screen.getByText(num + " = "); // beginning of a factorization message
+    }
+    expect(feedback).toBeInTheDocument();
+  });
+
+  it ("passes if a mnemonic appears after a wrong guess", async () => {
+    render(<Page />);
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText('Start'));
+    const num = parseInt(document.getElementById('num').textContent);
+    if (isPrime(num)) {
+      await user.click(screen.getByText('No'));
+    }
+    else {
+      await user.click(screen.getByText('Yes'));
+    }
+    // expect the divisibilityFeedback element to be visible
+    const feedback = document.getElementById('divisibilityFeedback');
+    expect(feedback.style.display).toBe("block");
+  });
 });
