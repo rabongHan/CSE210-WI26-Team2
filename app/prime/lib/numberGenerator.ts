6133396be3008fb67b7d5106380a5e8ab30242c3
Eric Binnendyk <ericabq@gmail.com>
2026-02-25 16:44:13 -0800

    clarify use of 0.000001 in getMinCountTests

--- app/prime/lib/numberGenerator.ts
+++ app/prime/lib/numberGenerator.ts
@@ -147,13 +147,16 @@ function getMinCountTests(
   let best = Infinity;
   const out: Test[] = [];
   for (const t of tests) {
-    // instead of minimizing over test counts, we minimize over testCount[testKey(t)]/testFreq[testKey(t)]
+    // Instead of minimizing over test counts, we minimize over testCount[testKey(t)]/testFreq[testKey(t)]
+    // The distribution weights are rational, and the ratios will also be rational, but they are stored as floating points.
+    // If two floating point values differ by less than 0.000001, we assume they
+    // represent the same rational number.
     const ratio = testCount[testKey(t)] / (testFreq[testKey(t)] ?? 1);
     if (ratio < best - 0.000001) {
       best = ratio;
       out.length = 0;
       out.push(t);
-    } else if (Math.abs(ratio - best) < 0.000001) { // we assume the two floating points represent the same (rational) number
+    } else if (Math.abs(ratio - best) < 0.000001) {
       out.push(t);
     }
   }
