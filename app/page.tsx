d6732cc870eee364cc7114bbf5dd40926c8e2bf2
Eric Binnendyk <ericabq@gmail.com>
2026-03-03 15:29:29 -0800

    begin writing code on homepage for detecting which games have been completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,16 @@
 import { Target } from "lucide-react";
 import { useEffect, useState } from "react";
 import {NavButton} from "@/app/bubble/components/bubble-buttons";
+import { loadPrimeEndGame } from "./save";
 
 export default function Page() {
   const [now, setNow] = useState("");
 
   useEffect(() => setNow(new Date().toISOString()), []);
+
+  const bubbleCompleted = true; // replace with actual game data
+  const treasureCompleted = true; // replace with actual game data
+  const primeCompleted = loadPrimeEndGame();
 
   return (
     <main style={{ fontFamily: "system-ui", padding: 24 }}>
@@ -25,6 +30,10 @@ export default function Page() {
           </NavButton>
       </div>
       <p>Time: {now}</p>
+      {/* Need to make this part of the page only appear when all three games are done */}
+      {bubbleCompleted && treasureCompleted && primeCompleted && (
+        <p>All games completed!</p>
+      )}
     </main>
   );
 }
