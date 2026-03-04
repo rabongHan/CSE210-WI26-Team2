38582f94ede2a44762d30770b118ea6c1dc3c1df
Eric Binnendyk <ericabq@gmail.com>
2026-03-03 19:00:31 -0800

    wrap load functions in try blocks and abstract away key names

--- app/prime/lib/save.ts
+++ app/prime/lib/save.ts
@@ -1,29 +1,39 @@
 // Save the game information in local storage after every round
+import { GameData } from "./types";
+
+const LS_WIN_STATUS_KEY = 'primeWon';
+const LS_KEY = 'primeGameData';
 
 export function saveEndGame(wonGame: boolean) {
     // whether or not the player won the prime testing game
-    localStorage.setItem('primeWon', btoa(wonGame.toString()));
+    localStorage.setItem(LS_WIN_STATUS_KEY, btoa(wonGame.toString()));
 }
 
-export function saveGameData(gameData) {
+export function saveGameData(gameData: GameData) {
     // save the game data in local storage, encoding it as a string
-    localStorage.setItem('primeGameData', btoa(JSON.stringify(gameData)));
+    localStorage.setItem(LS_KEY, btoa(JSON.stringify(gameData)));
 }
 
 export function loadEndGame() {
     // load whether or not the player won the prime testing game from local storage, decoding it from a string
-    const data = localStorage.getItem('primeWon');
-    if (data) {
-        return JSON.parse(atob(data));
+    try {
+        const data = localStorage.getItem(LS_WIN_STATUS_KEY);
+        if (data) {
+            return JSON.parse(atob(data));
+        }
+    } catch (e) {
+        return null;
     }
-    return null;
 }
 
 export function loadGameData() {
     // load the game data from local storage, decoding it from a string
-    const data = localStorage.getItem('primeGameData');
-    if (data) {
-        return JSON.parse(atob(data));
+    try {
+        const data = localStorage.getItem(LS_KEY);
+        if (data) {
+            return JSON.parse(atob(data));
+        }
+    } catch (e) {
+        return null; // probably means game data hasn't been saved yet, so just return null
     }
-    return null;
 }
